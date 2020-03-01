d88d74e394453e62df62de8f165004a7f77e2b03
Kennedy Osaze <kennedy-osaze@users.noreply.github.com>
2020-03-01 11:54:02 +0100

    Apply fixes from StyleCI (#2)

--- src/RandomJokesServiceProvider.php
+++ src/RandomJokesServiceProvider.php
@@ -3,13 +3,11 @@
 namespace KennedyOsaze\RandomJokes;
 
 use Illuminate\Support\ServiceProvider;
-use KennedyOsaze\RandomJokes\JokeFactory;
 
 class RandomJokesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
     }
 
     public function register()
